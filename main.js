6a04fcc69af1edd61821b1e2ead66e22eca7ba73
Kojoel <nanaabruquah1s@gmail.com>
2024-06-15 12:56:06 +0000

    Changed images used to links

--- main.js
+++ main.js
@@ -20,12 +20,16 @@ document.body.appendChild(next);
 
 
 let images = [
-    './Images original/thumbnails/Grassland-sm.jpg', 
-    './Images original/thumbnails/Hotairbaloons-sm.jpg', 
-    './Images original/thumbnails/Rockformation-sm.jpg', 
-    './Images original/thumbnails/Savannaplanes-sm.jpg', 
-    './Images original/thumbnails/Savannaplanesnight-sm.jpg', 
-    './Images original/thumbnails/sky-sm.jpg'];
+    'https://img.freepik.com/free-photo/green-field-tree-blue-skygreat-as-backgroundweb-banner-generative-ai_1258-152184.jpg?t=st=1718454413~exp=1718458013~hmac=46a959711d9bdf28188abd2468136f8a4b6ff4417c54d30972c3dbc6227cbbb9&w=826', 
+    'https://img.freepik.com/premium-photo/landscape-morning-fog-mountains-with-hot-air-balloons-sunrise_1029469-8968.jpg?w=826', 
+    'https://img.freepik.com/free-photo/beautiful-scenery-rock-formations-by-sea-queens-bath-kauai-hawaii-sunset_181624-36857.jpg?t=st=1718454281~exp=1718457881~hmac=32f9aa3c1cef1f8bc0784b1206236d4f59a168b10b14e375b009eb5fb41576bb&w=826', 
+    'https://img.freepik.com/free-photo/beautiful-shot-tree-savanna-plains-with-blue-sky_181624-21992.jpg?t=st=1718455044~exp=1718458644~hmac=ec5b36bdfe7def9961a4e327edecb994e48886e1dad1ab7cf018a2ac6954d704&w=826', 
+    'https://img.freepik.com/premium-photo/sunrise-maasai-mara-kenya-with-silhouette-tree_826551-1487.jpg?w=826', 
+    'https://img.freepik.com/free-photo/natural-landscape-with-sunrise_23-2149134291.jpg?t=st=1718455166~exp=1718458766~hmac=d1f7ad72c300918d15b0da30b5789cf0da73a0717e420c041f98488644cf7b8a&w=826',
+    'https://img.freepik.com/free-photo/breathtaking-view-ocean-cliffs-covered-with-plants-captured-lombok-indonesia_181624-8408.jpg?t=st=1718455272~exp=1718458872~hmac=273b96aee2834d733af49afe6fd7a68a488c5ccc229d4a13503a70b536aa2b04&w=826',
+    'https://img.freepik.com/free-photo/beautiful-aerial-shot-fronalpstock-mountains-switzerland-beautiful-pink-blue-sky_181624-9315.jpg?t=st=1718455334~exp=1718458934~hmac=0a6a38a917492c0903be1a36302b4f996824bfa7f917691d4b0bdbb4aed688c4&w=826',
+    'https://img.freepik.com/free-photo/beautiful-shot-snowy-mountains-with-dark-blue-sky_181624-2640.jpg?t=st=1718455476~exp=1718459076~hmac=5664ff5f6adca5bebbf09764d29dd8d55bd15abed9fe4b888ecadfc59665e712&w=826'
+];
 
 let captions = [
     'Grassland', 
@@ -33,7 +37,10 @@ let captions = [
     'Rock formation',
     'Savanna plains',
     'Savanna plains at night',
-    'Open sky'
+    'Open sky',
+    'Ocean and cliffs In Lombok',
+    'Fronalpstock mountains in Switzerland',
+    'snowy mountains'
 ];
 
 // console.log(images);
